dd5d253fcfc6657c02f264a4a0897c833ae5bcec
郭思义 <gsy@meitu.com>
2019-01-24 11:39:55 +0800

    [optimize] 添加交互

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class RandomWords extends StatefulWidget {
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
 
   Widget _buildSuggestions() {
     return new ListView.builder(
@@ -41,11 +42,25 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
@@ -57,7 +72,7 @@ class RandomWordsState extends State<RandomWords> {
 //    return _buildSuggestions();
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("startup Name Generator"),
+        title: new Text("startup Name Generor"),
       ),
       body: _buildSuggestions(),
     );
